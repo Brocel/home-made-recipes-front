5a0987459f1f557808961049255697f6d6599363
Brocel <geenens.prof@gmail.com>
2026-01-19 15:49:10 +0100

    refonte navbar

--- src/app/features/home/home/home.ts
+++ src/app/features/home/home/home.ts
@@ -1,19 +1,18 @@
-import { Component, OnInit, signal } from '@angular/core';
-import { TranslatePipe } from '@ngx-translate/core';
 import { CommonModule } from '@angular/common';
+import { Component, OnInit, signal } from '@angular/core';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { RouterModule } from '@angular/router';
 import { RecipesApi } from '@core/api/recipes.api';
-import { Recipe } from '@shared/models/recipes/recipe';
 import { isAuthenticated } from '@core/auth/auth.signals';
-import { QuickSearch } from '@features/recipes/search/quick-search/quick-search';
-import { AdvancedSearch } from '@features/recipes/search/advanced-search/advanced-search';
-import { AuthCallToAction } from '../auth-call-to-action/auth-call-to-action';
+import { LanguageService } from '@core/i18n/language.service';
 import { AddRecipeMini } from '@features/recipes/add/add-recipe-mini/add-recipe-mini';
+import { RecipeMini } from '@features/recipes/recipe-mini/recipe-mini';
+import { QuickSearch } from '@features/recipes/search/quick-search/quick-search';
+import { TranslatePipe } from '@ngx-translate/core';
+import { Recipe } from '@shared/models/recipes/recipe';
 import { finalize, of, take, tap } from 'rxjs';
 import { catchError } from 'rxjs/operators';
-import { RecipeMini } from '@features/recipes/recipe-mini/recipe-mini';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { LanguageService } from '@core/i18n/language.service';
+import { AuthCallToAction } from '../auth-call-to-action/auth-call-to-action';
 
 @Component({
   selector: 'app-home',
@@ -22,7 +21,6 @@ import { LanguageService } from '@core/i18n/language.service';
     CommonModule,
     RouterModule,
     QuickSearch,
-    AdvancedSearch,
     AuthCallToAction,
     AddRecipeMini,
     TranslatePipe,
@@ -30,13 +28,14 @@ import { LanguageService } from '@core/i18n/language.service';
     MatProgressSpinnerModule,
   ],
   templateUrl: './home.html',
-  styleUrls: ['./home.scss']
+  styleUrls: ['./home.scss'],
 })
 export class Home implements OnInit {
-  fakeDailyRecipe: Recipe = { // TODO: delete later
+  fakeDailyRecipe: Recipe = {
+    // TODO: delete later
     id: 0,
     title: 'Recette du jour',
-    description: 'Une délicieuse recette à essayer aujourd\'hui.',
+    description: "Une délicieuse recette à essayer aujourd'hui.",
     author: 'Chef Exemple',
     type: 'Dessert',
     preparationTime: 30,
@@ -48,9 +47,7 @@ export class Home implements OnInit {
   auth = isAuthenticated;
   errorMessage = signal<string | null>(null);
 
-  constructor(private api: RecipesApi, private lang: LanguageService) {
-  }
-
+  constructor(private api: RecipesApi, private lang: LanguageService) {}
 
   ngOnInit(): void {
     this.fetchDailyRecipe();
@@ -61,20 +58,23 @@ export class Home implements OnInit {
 
   fetchDailyRecipe(): void {
     this.loading.set(true);
-    this.api.dailyRecipe().pipe(
-      take(1),
-      tap((recipe: Recipe) => {
-        this.dailyRecipe.set(recipe);
-      }),
-      catchError(() => {
-        this.lang.setMsg('errors.recipe.fetchingDaily', undefined);
-        this.errorMessage = this.lang.getMessageSignal();
-        return of(null);
-      }),
-      finalize(() => {
-        this.loading.set(false);
-      })
-    ).subscribe();
+    this.api
+      .dailyRecipe()
+      .pipe(
+        take(1),
+        tap((recipe: Recipe) => {
+          this.dailyRecipe.set(recipe);
+        }),
+        catchError(() => {
+          this.lang.setMsg('errors.recipe.fetchingDaily', undefined);
+          this.errorMessage = this.lang.getMessageSignal();
+          return of(null);
+        }),
+        finalize(() => {
+          this.loading.set(false);
+        })
+      )
+      .subscribe();
   }
 
   onSearch(query: string) {
