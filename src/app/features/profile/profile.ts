614ace06fe52a70620afc55c52a24fc1dd64ed1d
Brocel <geenens.prof@gmail.com>
2026-02-26 15:47:53 +0100

    completing profile structure

--- src/app/features/profile/profile.ts
+++ src/app/features/profile/profile.ts
@@ -1,20 +1,39 @@
 import { Component, inject, OnInit, signal } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { AuthService } from '@app/core/services/auth.service';
+import { UsernameValidator } from '@app/core/validators/username.validator';
 import { User } from '@app/shared/models/user';
 import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-profile',
-  imports: [TranslatePipe],
+  imports: [ReactiveFormsModule, TranslatePipe],
   templateUrl: './profile.html',
   styleUrl: './profile.scss',
 })
 export class Profile implements OnInit {
   private auth = inject(AuthService);
+  private fb = inject(FormBuilder);
+  private usernameValidator = inject(UsernameValidator);
 
   user: User | null = null;
 
   isEditing = signal(false);
+  loading = signal(false);
+  errorKey = signal<string | null>(null);
+
+  form = this.fb.nonNullable.group({
+    first_name: this.fb.nonNullable.control('', { validators: [Validators.required] }),
+    last_name: this.fb.nonNullable.control('', { validators: [Validators.required] }),
+    username: this.fb.nonNullable.control('', {
+      validators: [Validators.required],
+      asyncValidators: [this.usernameValidator.validate.bind(this.usernameValidator)],
+      updateOn: 'blur',
+    }),
+    birth_date: this.fb.nonNullable.control('', {
+      validators: [Validators.required],
+    }),
+  });
 
   ngOnInit(): void {
     this.user = this.auth.getCurrentUser();
@@ -23,4 +42,8 @@ export class Profile implements OnInit {
   startEditing() {
     this.isEditing.set(true);
   }
+
+  updateUserInfo(): void {}
+
+  updateAvatar(): void {}
 }
